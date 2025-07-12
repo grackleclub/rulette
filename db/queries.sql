6873497aca5caab6de720e69ed6e721867ef47e0
turk <turk@grackle.club>
2025-07-11 22:25:18 -0600

    broken, but progressing schema

--- db/queries.sql
+++ db/queries.sql
@@ -1,42 +1,139 @@
 -- name: PlayerCreate :exec
 INSERT INTO players (name) VALUES (:name) RETURNING id;
 
--- Player
--- PlayerDelete
+-- name: Player :one
+SELECT * FROM players WHERE id = :id;
 
--- CardCreate
--- Card
--- CardEdit
--- CardDelete
+-- name: PlayerDelete :exec
+DELETE FROM players WHERE id = :id;
 
+-- name: CardCreate :exec
+INSERT INTO cards (type, front, back, creator)
+VALUES (:type, :front, :back, :creator)
+RETURNING id;
+
+-- name: Card :one
+SELECT * FROM cards WHERE id = :id;
+-- name: CardDelete :exec
+DELETE FROM cards WHERE id = :id;
 
 -- name: GameCreate :exec
-INSERT INTO games (name, code, owner)
-VALUES (:name, :code, :owner) 
-RETURNING code;
--- Game
--- GameEdit
--- GameDelete
--- GameActiveCount
+INSERT INTO games (name, id, owner_id)
+VALUES (:name, :id, :owner_id) 
+RETURNING id;
+-- name: Games :many
+SELECT * FROM games WHERE code = (
+	SELECT game_id 
+	FROM game_players
+	WHERE player_id = :player_id
+);
+-- name: GameDelete :exec
+DELETE FROM games WHERE id = :id;
+
 -- GameState  TODO: maybe deliver state in several queries?
 
 -- GameCardCreate
--- SpinThatWheel
--- GameCardMove
--- GameCardReveal
--- GameCardFlip
+
+-- -- SpinThatWheel :exec
+-- WITH slots AS (
+-- 	SELECT MAX(slot) 
+-- 	FROM game_cards 
+-- 	WHERE game_id = :game_id
+-- ),
+-- spin AS (
+-- 	SELECT card_id
+-- 	FROM game_cards
+-- 	WHERE game_id = :game_id
+-- 		AND revealed = false
+-- 		AND slot = RANDOM() % (SELECT * FROM slots)
+-- 	ORDER BY stack DESC LIMIT 1
+-- )
+-- UPDATE game_cards
+-- SET	
+-- 	revealed = true, 
+-- 	player_id = :player_id
+-- WHERE id = (SELECT card_id FROM spin);
+
+-- Moves a single card of matching id to the new player_id provided.
+-- name: GameCardMove :exec
+UPDATE game_cards
+SET player_id = :player_id
+WHERE game_id = :game_id
+	AND card_id = :card_id
+	AND id = (
+		SELECT id
+		FROM game_cards
+		WHERE game_id = :game_id AND card_id = :card_id
+		LIMIT 1
+	)	
+;
+-- name: GameCardFlip :exec
+UPDATE game_cards
+SET flipped = NOT flipped
+WHERE game_id = :game_id
+	AND card_id = :card_id
+	AND id = (
+		SELECT id
+		FROM game_cards
+		WHERE game_id = :game_id AND card_id = :card_id
+		LIMIT 1
+	)
+;
+
 -- GameCardClone
--- GameCardShred
--- GameCardDelete
 
--- GamePlayerCreate
--- GamePlayer
--- GamePlayerEdit
--- GamePlayerDelete
--- GamePlayerPoints
+-- name: GameCardShred :exec
+UPDATE game_cards
+SET shredded = true
+WHERE game_id = :game_id
+	AND card_id = :card_id
+	AND id = (
+		SELECT id
+		FROM game_cards
+		WHERE game_id = :game_id AND card_id = :card_id
+		LIMIT 1
+	)
+;
 
--- TurnOrderCreate
--- TurnOrderEdit
+-- name: GamePlayerCreate :exec
+INSERT INTO game_players (game_id, player_id)
+VALUES (:game_id, :player_id);
+
+-- name: GamePlayerDelete :exec
+DELETE FROM game_players 
+WHERE game_id = :game_id 
+	AND player_id = :player_id;
+
+-- name: GamePlayerPoints :many
+SELECT (
+	SELECT name FROM players WHERE id=player_id
+	), 
+	points,
+	turn_active
+FROM game_players 
+WHERE game_id = :game_id;
+
+-- name: TurnOrderInit :exec
+UPDATE game_players
+SET turn_active = true
+WHERE game_id = :game_id
+	AND player_id = (
+		SELECT id 
+		FROM players 
+		ORDER BY joined ASC
+		LIMIT 1
+	)
+;
+
+-- name: TurnOrderAdvance :exec
+-- WITH players AS (
+-- 	SELECT COUNT(player_id) AS count
+-- 	FROM game_players
+-- 	WHERE is_host = false 
+-- ),
+-- TODO: fix this
+;
+
 -- TurnOrderDelete
 -- TurnOrderNext
 
--- db/schema.sql
+++ db/schema.sql
@@ -4,6 +4,26 @@ CREATE TABLE IF NOT EXISTS players (
 	created TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
+CREATE TABLE IF NOT EXISTS games (
+	name TEXT NOT NULL,
+	id VARCHAR(6) PRIMARY KEY,
+	created TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+	owner_id INT NOT NULL,
+	FOREIGN KEY (owner_id) REFERENCES players(id) ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS game_players (
+	game_id INT NOT NULL,
+	player_id INT NOT NULL,
+	points INT DEFAULT 20,
+	is_host BOOLEAN DEFAULT FALSE,
+	joined TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+	turn_active BOOLEAN DEFAULT FALSE,
+	PRIMARY KEY (game_id, player_id),
+	FOREIGN KEY (game_id) REFERENCES games(id) ON DELETE CASCADE,
+	FOREIGN KEY (player_id) REFERENCES players(id) ON DELETE CASCADE
+);
+
 -- TODO: should is_host be a card?
 CREATE TABLE IF NOT EXISTS card_types (
 	name TEXT NOT NULL UNIQUE
@@ -19,23 +39,16 @@ CREATE TABLE IF NOT EXISTS cards (
 	type TEXT NOT NULL,
 	front TEXT NOT NULL,
 	back TEXT,
-	created TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 	creator INTEGER,
+	created TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 	default BOOLEAN DEFAULT FALSE,
 	FOREIGN KEY (type) REFERENCES card_types(name) ON DELETE CASCADE,
 	FOREIGN KEY (creator) REFERENCES players(id)
 );
 
-CREATE TABLE IF NOT EXISTS games (
-	name TEXT NOT NULL,
-	code VARCHAR(6) PRIMARY KEY,
-	created TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-	owner INT NOT NULL,
-	FOREIGN KEY (owner) REFERENCES players(id) ON DELETE CASCADE
-);
-
 -- no primary key because cloning a card is possible
 CREATE TABLE IF NOT EXISTS game_cards (
+	id INTEGER PRIMARY KEY AUTOINCREMENT, -- to distinguish between clones
 	game_id VARCHAR(6) NOT NULL,
 	card_id INT NOT NULL,
 	slot INT NOT NULL, -- 1-indexed number of wheel slots
@@ -44,29 +57,10 @@ CREATE TABLE IF NOT EXISTS game_cards (
 	revealed BOOLEAN DEFAULT FALSE, -- on the wheel
 	flipped BOOLEAN DEFAULT FALSE,
 	shredded BOOLEAN DEFAULT FALSE,
-	FOREIGN KEY (game_id) REFERENCES games(code) ON DELETE CASCADE,
+	from_clone BOOLEAN DEFAULT FALSE,
+	FOREIGN KEY (game_id) REFERENCES games(id) ON DELETE CASCADE,
 	FOREIGN KEY (card_id) REFERENCES cards(id) ON DELETE CASCADE,
 	FOREIGN KEY (player_id) REFERENCES players(id)
-);
-
-CREATE TABLE IF NOT EXISTS game_players (
-	game_id INT NOT NULL,
-	player_id INT NOT NULL,
-	points INT DEFAULT 20,
-	is_host BOOLEAN DEFAULT FALSE,
-	PRIMARY KEY (game_id, player_id),
-	FOREIGN KEY (game_id) REFERENCES games(id) ON DELETE CASCADE,
-	FOREIGN KEY (player_id) REFERENCES players(id) ON DELETE CASCADE
-);
-
-CREATE TABLE IF NOT EXISTS turn_order (
-	game_id INT NOT NULL,
-	player_id INT NOT NULL,
-	position INT NOT NULL,
-	active BOOLEAN DEFAULT FALSE,
-	PRIMARY KEY (game_id, player_id),
-	FOREIGN KEY (game_id) REFERENCES games(code) ON DELETE CASCADE,
-	FOREIGN KEY (player_id) REFERENCES players(id) ON DELETE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS infractions (
@@ -77,7 +71,7 @@ CREATE TABLE IF NOT EXISTS infractions (
 	created TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 	active BOOLEAN DEFAULT TRUE, -- active until applied or denied
 	convicted BOOLEAN DEFAULT FALSE,
-	FOREIGN KEY (game_id) REFERENCES games(code) ON DELETE CASCADE,
+	FOREIGN KEY (game_id) REFERENCES games(id) ON DELETE CASCADE,
 	FOREIGN KEY (accused) REFERENCES players(id) ON DELETE CASCADE,
 	FOREIGN KEY (accuser) REFERENCES players(id) ON DELETE CASCADE,
 	PRIMARY KEY (game_id, accused, accuser)
