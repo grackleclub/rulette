02a935433768627acedb8192c3e96363498aa331
turk <turk@grackle.club>
2025-07-18 21:06:51 -0600

    add postgres

--- main.go
+++ main.go
@@ -1,19 +1,35 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log/slog"
 	"net/http"
+
+	"github.com/grackleclub/postgres"
 )
 
 var portDefault = 7777
 
 func main() {
-	slog.Info("starting server", "port", portDefault)
-
 	mux := http.NewServeMux()
 	mux.HandleFunc("/state", stateHandler)
+	ctx := context.Background()
+	opts := postgres.PostgresOpts{
+		Host:     "localhost",
+		User:     "postgres",
+		Password: "TODO:replace-temporary",
+		Port:     "5432",
+		Sslmode:  "disable",
+	}
+	db, close, err := postgres.NewTestDB(ctx, opts)
+	if err != nil {
+		panic(fmt.Sprintf("create test database: %v", err))
+	}
+	defer close()
+	slog.Info("created test database", "db", db)
 
+	slog.Info("starting server", "port", portDefault)
 	http.ListenAndServe(fmt.Sprintf(":%d", portDefault), mux)
 
 	slog.Info("all done")
