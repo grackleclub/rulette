6730407896186bfea62b85f36c9e2a8552ca1740
kat <k.lukens@gmail.com>
2026-02-19 11:06:46 -0700

    Revert "implement database actions like spin (#12)"
    This reverts commit 816b217bb93d9a0d00b42730a074e9db97ff38e7.

--- db/schema.sql
+++ db/schema.sql
@@ -10,28 +10,26 @@ CREATE TABLE IF NOT EXISTS game_states (
 	description TEXT
 );
 
-INSERT INTO game_states (id, name, description)
-VALUES
-(0, 'created', 'game created, but no members have joined'),
-(1, 'inviting', 'at least one player has joined'), --  TODO: useless?
-(2, 'ready', 'joining is closed, ready to start (or paused)'),
-(3, 'turn', 'player is mid-turn, spinning wheel or responding'),
-(4, 'challenge', ''), -- pause for points adjustment |  TODO: useless?
-(5, 'end', 'game over');
-
 CREATE TABLE IF NOT EXISTS games (
 	id VARCHAR(6) PRIMARY KEY,
 	name TEXT NOT NULL,
 	created TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 	owner_id INTEGER,
 	state_id INTEGER NOT NULL DEFAULT 0,
-	wheel_slots INTEGER NOT NULL DEFAULT 2, -- number of wheel possibilities -- TODO: fill out
-	card_count INTEGER NOT NULL DEFAULT 10, -- number of cards in the wheel deck -- TODO: fill out
-	initiative_timer INTEGER NOT NULL DEFAULT 30, -- seconds per turn before auto-advance
 	initiative_current INTEGER DEFAULT 0, -- TODO: is this used?
 	FOREIGN KEY (owner_id) REFERENCES players(id) ON DELETE CASCADE,
 	FOREIGN KEY (state_id) REFERENCES game_states(id)
 );
+
+INSERT INTO game_states (id, name, description)
+VALUES
+	(0, 'created', 'game created, but no members have joined'),
+	(1, 'inviting', 'at least one player has joined'), --  TODO: useless?
+	(2, 'ready', 'joining is closed, ready to start (or paused)'),
+	(3, 'turn', 'player is mid-turn, spinning wheel or responding'),
+	(4, 'challenge', ''), -- pause for points adjustment |  TODO: useless?
+	(5, 'end', 'game over')
+;
 
 CREATE TABLE IF NOT EXISTS game_players (
 	game_id VARCHAR(6) NOT NULL,
@@ -72,34 +70,20 @@ VALUES
 	('rule', 'wearing a giant hat', 'wearing a tiny hat', 0, CURRENT_TIMESTAMP, TRUE),
 	('rule', 'while doing your best Robert Dinero impersonation', 'wearing a tiny hat', 0, CURRENT_TIMESTAMP, TRUE);
 
--- card_id lacks primary key to allow cloning within a game,
+-- no primary key because cloning a card is possible
 CREATE TABLE IF NOT EXISTS game_cards (
 	id SERIAL PRIMARY KEY, -- to distinguish between clones
 	game_id VARCHAR(6) NOT NULL,
 	card_id INTEGER NOT NULL,
-	slot INTEGER, -- 1-indexed number of wheel slots (MAX=game.wheel_size, NULL=revealed)
-	stack INTEGER, -- 1-indexed ascending up the stack (NULL=unshuffled)
-	player_id INTEGER, -- (NULL=on the wheel)
+	slot INTEGER NOT NULL, -- 1-indexed number of wheel slots
+	stack INTEGER NOT NULL, -- 0 bottom, 1 middle, 2 top; irrelevant if revealed
+	player_id INTEGER, -- only populated when revealed=true
+	revealed BOOLEAN DEFAULT FALSE, -- on the wheel
 	flipped BOOLEAN DEFAULT FALSE,
 	shredded BOOLEAN DEFAULT FALSE,
-	from_clone BOOLEAN DEFAULT FALSE, -- TODO: this can be inferred, why put it here?
-	updated TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+	from_clone BOOLEAN DEFAULT FALSE,
 	FOREIGN KEY (game_id) REFERENCES games(id) ON DELETE CASCADE,
-	FOREIGN KEY (card_id) REFERENCES cards(id) ON DELETE CASCADE, -- allows global deletion, admin moderation
-	FOREIGN KEY (player_id) REFERENCES players(id) ON DELETE CASCADE -- a leaving player takes their game cards with them
-);
-
--- TODO: make log viewer route?
-CREATE TABLE IF NOT EXISTS spin_log (
-	id SERIAL PRIMARY KEY,
-	game_id VARCHAR(6) NOT NULL,
-	player_id INTEGER, -- (NULL=system,deleted)
-	slot INTEGER NOT NULL, -- not referentially enforced, oh well
-	card_id INTEGER, -- (NULL=miss,?)
-	ts TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-	FOREIGN KEY (game_id) REFERENCES games(id) ON DELETE CASCADE,
-	FOREIGN KEY (player_id) REFERENCES players(id) ON DELETE SET NULL,
-	FOREIGN KEY (card_id) REFERENCES cards(id) ON DELETE SET NULL
+	FOREIGN KEY (card_id) REFERENCES cards(id) ON DELETE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS infractions (
@@ -115,8 +99,3 @@ CREATE TABLE IF NOT EXISTS infractions (
 	FOREIGN KEY (accuser) REFERENCES players(id) ON DELETE CASCADE
 );
 
-CREATE UNLOGGED TABLE IF NOT EXISTS game_cache (
-	game_id VARCHAR(6) PRIMARY KEY,
-	value JSONB,
-	expires TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP + INTERVAL '1 seconds' -- TODO: make var?
-);
