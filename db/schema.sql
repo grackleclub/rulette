e32397cb0fe8d0470dac1590956e93fdb43fca53
kat <k.lukens@gmail.com>
2026-05-19 16:50:53 -0600

    sql tweaks to prevent card duplication

--- db/schema.sql
+++ db/schema.sql
@@ -75,7 +75,7 @@ ON CONFLICT DO NOTHING;
 CREATE TABLE IF NOT EXISTS cards (
 	id SERIAL PRIMARY KEY,
 	type TEXT NOT NULL,
-	front TEXT NOT NULL UNIQUE,
+	front TEXT NOT NULL,
 	back TEXT,
 	creator INTEGER,
 	created TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
@@ -89,9 +89,6 @@ CREATE TABLE IF NOT EXISTS cards (
 	)
 );
 CREATE UNIQUE INDEX IF NOT EXISTS cards_front_unique ON cards (front);
-
--- TODO: Don't
-DELETE FROM cards WHERE generic = TRUE;
 
 INSERT INTO cards (type, front, back, creator, created, generic, modifier_effect)
 VALUES
@@ -107,10 +104,7 @@ VALUES
 	('rule', 'while singing', 'in a monotone', 0, CURRENT_TIMESTAMP, TRUE, NULL),
 	('rule', 'in your best Shakespearian english', 'using all the contemporary slang you can', 0, CURRENT_TIMESTAMP, TRUE, NULL),
 	('rule', 'never saying "um"', 'saying "um" every other word', 0, CURRENT_TIMESTAMP, TRUE, NULL),
-	-- ('rule', 'never using your hands', 'vogueing', 0, CURRENT_TIMESTAMP, TRUE, NULL),
 	('rule', 'doing your best Matthew McConaughey impersonation', 'doing your worst Jack Nicholson impersonation', 0, CURRENT_TIMESTAMP, TRUE, NULL),
-	-- ('rule', 'without smiling', 'always smiling', 0, CURRENT_TIMESTAMP, TRUE, NULL),
-	-- ('rule', 'with your eyes closed', 'staring intently', 0, CURRENT_TIMESTAMP, TRUE, NULL),
 	('rule', 'as if everything is juicy gossip', 'as if everything is really boring', 0, CURRENT_TIMESTAMP, TRUE, NULL),
 	('rule', 'while trying to incite a revolution', 'while trying to calm everyone down', 0, CURRENT_TIMESTAMP, TRUE, NULL),
 	('rule', 'like your mouth is full of marshmallows', 'like you have horrible cottonmouth', 0, CURRENT_TIMESTAMP, TRUE, NULL),
@@ -122,7 +116,38 @@ VALUES
 	('rule', 'assigning superlatives to the other players', 'assigning superlatives to yourself', 0, CURRENT_TIMESTAMP, TRUE, NULL),
 	('rule', 'as if everything is a question', 'as if everything is a definite answer', 0, CURRENT_TIMESTAMP, TRUE, NULL),
 	('rule', 'with vocal fry', 'over-enunciating', 0, CURRENT_TIMESTAMP, TRUE, NULL)
-ON CONFLICT (front) DO NOTHING;
+ON CONFLICT (front) DO UPDATE SET
+	back = EXCLUDED.back,
+	type = EXCLUDED.type,
+	generic = EXCLUDED.generic,
+	modifier_effect = EXCLUDED.modifier_effect;
+
+DELETE FROM cards WHERE generic = TRUE AND front NOT IN (
+	'flip any of your own cards',
+	'shred any of your own cards',
+	'clone any of your own cards, and give to someone else',
+	'transfer any of your own cards to another player',
+	'in a whisper',
+	'doing your best Robert De Nero impersonation',
+	'not using any pronouns',
+	'referring to yourself only in the third person',
+	'in a transatlantic accent',
+	'while singing',
+	'in your best Shakespearian english',
+	'never saying "um"',
+	'doing your best Matthew McConaughey impersonation',
+	'as if everything is juicy gossip',
+	'while trying to incite a revolution',
+	'like your mouth is full of marshmallows',
+	'name dropping every sentence',
+	'speaking only in haiku',
+	'always starting with a compliment',
+	'starting every sentence with the next letter of the alphabet',
+	'starting with a fun fact',
+	'assigning superlatives to the other players',
+	'as if everything is a question',
+	'with vocal fry'
+);
 
 -- card_id lacks primary key to allow cloning within a game,
 CREATE TABLE IF NOT EXISTS game_cards (
